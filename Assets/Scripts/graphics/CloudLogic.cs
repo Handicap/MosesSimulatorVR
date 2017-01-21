81737a3aa786988614e30e0bbc82e6133a1924a6
Samuli Handicap Kinnunen <sambzalgod_32@hotmail.com>
2017-01-21 17:35:18 +0200

    Failed cloud stuff

---
+++ Assets/Scripts/graphics/CloudLogic.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudLogic : MonoBehaviour {
+
+	// fading in and out times (seconds)
+	public float fadeInTime = 3f;
+	public float fadeOutTime = 5;
+
+	// direction to go and speed constraints
+	public Vector3 direction = Vector3.forward;
+	public float minSpeed = 0.2f;
+	public float maxSpeed = 1f;
+
+	private float speed;
+
+	// minimum and maximum lifetime
+	public float maxLife = 5f;
+	public float minLife = 2f;
+	private float currentLife;
+	private float deathTime;
+
+	public float minScale = 0.5f;
+	public float maxScale = 3f;
+
+	public int blendShapes = 4;
+
+	private Material cloudmat;
+
+		
+	void Start(){
+		speed = Random.Range (minSpeed, maxSpeed);
+		deathTime = Random.Range (minLife, maxLife);
+		transform.localScale = Vector3.one * Random.Range (minScale, maxScale);
+
+		SkinnedMeshRenderer skinrend = transform.GetChild(0).GetComponent <SkinnedMeshRenderer>();
+
+		for (int i = 0; i < blendShapes; i++) {
+			skinrend.SetBlendShapeWeight(i, Random.Range(0f, 100f));
+		}
+
+		transform.localRotation = Quaternion.Euler (0, Random.Range (0, 359), 0);
+
+		cloudmat = skinrend.material;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = transform.position + (direction * speed);
+		currentLife = currentLife + Time.deltaTime;
+
+		if (currentLife > deathTime) {
+			Destroy (gameObject);
+		}
+
+
+	}
+
+	private Color ChangeAlpha(Color original, float alpha){
+		Color newcol = new Color(cloudmat.color.r, cloudmat.color.g, cloudmat.color.b, alpha);
+		return newcol;
+	}
+
+}
---
+++ Assets/Scripts/graphics/CloudSpawner.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Generates cloud objects as a point of reference/graphical thingamajick
+/// </summary>
+public class CloudSpawner : MonoBehaviour {
+
+	public GameObject cloud_prefab;
+
+	public float minSpawnFreq = 2f;
+	public float maxSpawnFreq = 5f;
+	public float nextPhase;
+	public float currentTime = 0f;
+
+	private GameObject corner1;
+	private GameObject corner2;
+	private GameObject cloudpool;
+
+	public Vector3 cloudTravelVector = Vector3.forward;
+
+	void Start(){
+		nextPhase = minSpawnFreq;
+		corner1 = transform.GetChild (0).gameObject;
+		corner2 = transform.GetChild (1).gameObject;
+		cloudpool = transform.GetChild (2).gameObject;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		currentTime = currentTime + Time.deltaTime;
+		if (currentTime > nextPhase) {
+		
+			nextPhase = Random.Range (minSpawnFreq, maxSpawnFreq);
+			Vector3 spawnPos = GenerateSpawnPos ();
+			GameObject newcloud = Instantiate (cloud_prefab, spawnPos, Quaternion.identity, cloudpool.transform);
+			CloudLogic newcloudLog = newcloud.GetComponent <CloudLogic>();
+			newcloudLog.direction = cloudTravelVector;
+			currentTime = 0f;
+
+		}
+	}
+
+	/// <summary>
+	/// Generates a new vector3 by randomizing between the corner positions
+	/// </summary>
+	/// <returns>Vector3 that is the new spawn position.</returns>
+	Vector3 GenerateSpawnPos(){
+
+		Vector3 newpos = Vector3.zero;
+
+		newpos.x = Random.Range (corner1.transform.position.x, corner2.transform.position.x);
+		newpos.y = Random.Range (corner1.transform.position.y, corner2.transform.position.y);
+		newpos.z = Random.Range (corner1.transform.position.z, corner2.transform.position.z);
+
+		return newpos;
+	}
+}

2c1e4e44695fddfaf1a8b4d3383a48b30f6352dd
Samuli Ranta-Niikku <fhskjfhskfhkjsfdksj@bfkjsdhfkjshfjks.com>
2017-01-21 11:52:55 +0200

    SeeS

--- Assets/Scripts/WaterScript.cs
+++ Assets/Scripts/WaterScript.cs
@@ -9,17 +9,29 @@ using UnityEngine.UI;
 public class WaterScript : MonoBehaviour {
 
 	private Mesh mesh;
-	private Vector3[] vertices;
-	private Vector3[] normals;
+	public Vector3[] vertices;
+	public Vector3[] normals;
 
 	public GameObject player;
-
-	private Vector3 initialHeight;
-
+	public float arith;
+	public Vector3 initialHeight;
 
 	void Start () {
+
+		mesh = GetComponent<MeshFilter> ().mesh;
+		vertices = mesh.vertices;
 		initialHeight = vertices [0];
+
+
+		
+		mesh = GetComponent<MeshFilter> ().mesh;
+		vertices = mesh.vertices;
+		normals = mesh.normals;
+
+
 	}
+
+	private List<float> arithmetic;
 
 	void Update () {
 		
@@ -27,15 +39,18 @@ public class WaterScript : MonoBehaviour {
 		vertices = mesh.vertices;
 		normals = mesh.normals;
 
-
 		int i = 0;
+
 		while (i < vertices.Length) {
 			
 			if (Vector3.Distance (vertices [i], player.transform.position) < 2f) {
 				vertices [i] -= normals [i];
+
 			} else if (vertices [i].y < initialHeight.y) {
+
 				vertices [i] += normals [i] / 100f;
 			}
+
 			i++;
 		}
 		mesh.vertices = vertices;
